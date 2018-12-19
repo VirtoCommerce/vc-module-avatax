418aaf30789d74bc4e14008ef10c0534c5f01da9
Eugeny Tatarincev <et@virtoway.com>
2018-12-19 16:39:54 +0200

    2.14.0

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -13,10 +13,10 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("VirtoCommerce")]
 [assembly: AssemblyProduct("Virto Commerce Avalara.Tax Module")]
-[assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2016")]
+[assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2018")]
 
-[assembly: AssemblyFileVersion("2.13.2.0")]
-[assembly: AssemblyVersion("2.13.2.0")]
+[assembly: AssemblyFileVersion("2.14.0.0")]
+[assembly: AssemblyVersion("2.14.0.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
