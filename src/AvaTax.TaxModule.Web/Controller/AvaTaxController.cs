cf48523dc5d8540a4dd71ff1d6a8bd7901cfc385
Aleksandr Andreev <alivemenone@gmail.com>
2020-02-06 17:05:33 +0300

    rename retVal variable to result

--- src/AvaTax.TaxModule.Web/Controller/AvaTaxController.cs
+++ src/AvaTax.TaxModule.Web/Controller/AvaTaxController.cs
@@ -1,4 +1,4 @@
-﻿using Avalara.AvaTax.RestClient;
+using Avalara.AvaTax.RestClient;
 using AvaTax.TaxModule.Core;
 using AvaTax.TaxModule.Core.Models;
 using AvaTax.TaxModule.Core.Services;
@@ -48,63 +48,63 @@ namespace AvaTax.TaxModule.Web.Controller
         [ProducesResponseType(typeof(void), StatusCodes.Status204NoContent)]
         public Task<ActionResult> TestConnection([FromBody]AvaTaxSettings taxSetting)
         {
-            ActionResult retVal = BadRequest();
+            ActionResult result = BadRequest();
             LogInvoker<AvalaraLogger.TaxRequestContext>.Execute(log =>
             {
                 if (taxSetting == null)
                 {
                     const string errorMessage = "The connectionInfo parameter is required to test the connection.";
-                    retVal = BadRequest(errorMessage);
+                    result = BadRequest(errorMessage);
                     throw new Exception(errorMessage);
                 }
 
                 if (!taxSetting.IsEnabled)
                 {
                     const string errorMessage = "Tax calculation disabled, enable before testing connection.";
-                    retVal = BadRequest(errorMessage);
+                    result = BadRequest(errorMessage);
                     throw new Exception(errorMessage);
                 }
 
                 if (!taxSetting.IsValid)
                 {
                     const string errorMessage = "AvaTax credentials are not provided.";
-                    retVal = BadRequest(errorMessage);
+                    result = BadRequest(errorMessage);
                     throw new Exception(errorMessage);
                 }
 
                 var avaTaxClient = _avaTaxClientFactory(taxSetting);
 
-                PingResultModel result;
+                PingResultModel pingResult;
                 try
                 {
-                    result = avaTaxClient.Ping();
+                    pingResult = avaTaxClient.Ping();
                 }
                 catch (JsonException e)
                 {
                     var errorMessage = $"Avalara API service responded with some unexpected data. Please verify the link to Avalara API service.\nInner error: {e.Message}";
-                    retVal = BadRequest(errorMessage);
+                    result = BadRequest(errorMessage);
                     throw new Exception(errorMessage, e);
                 }
                 catch (Exception e)
                 {
                     var errorMessage = $"Failed to connect to the Avalara API due to error: {e.Message}";
-                    retVal = BadRequest(errorMessage);
+                    result = BadRequest(errorMessage);
                     throw new Exception(errorMessage, e);
                 }
 
-                if (result.authenticated != true)
+                if (pingResult.authenticated != true)
                 {
                     var errorMessage = "Provided Avalara credentials are invalid. Please verify the account number and the license key.";
-                    retVal = BadRequest(errorMessage);
+                    result = BadRequest(errorMessage);
                     throw new Exception(errorMessage);
                 }
 
-                retVal = Ok(result);
+                result = Ok(pingResult);
             })
             .OnError(_logger, AvalaraLogger.EventCodes.TaxPingError)
             .OnSuccess(_logger, AvalaraLogger.EventCodes.Ping);
 
-            return Task.FromResult(retVal);
+            return Task.FromResult(result);
         }
 
         [HttpPost]
